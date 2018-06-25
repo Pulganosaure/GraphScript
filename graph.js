c03c73abd6475b1ba3c40058bc377acf8855ff51
sc-pulgan <38398757+sc-pulgan@users.noreply.github.com>
2018-06-25 13:57:34 +0200

    update

--- graph.js
+++ graph.js
@@ -1,10 +1,40 @@
-const nbrvalue = 10;
+const nbrvalue = 5
 
+
+function get_BGSdata_from_API()
+{
+    $.get("https://www.edsm.net/api-system-v1/factions?systemName=Karka&showHistory=1", {})
+    .done(function(data)
+    {
+        series = []
+        for(var i = 0; i <= Object.keys(data).length; i++) 
+        {
+           influences_Data = data['factions'][i]['influenceHistory']
+           influences = []
+
+
+           for(var j in influences_Data)
+           {
+            influences.push(Number((influences_Data [j] * 100).toFixed()))
+           }
+            influences = influences.slice(influences.length - nbrvalue)
+            var faction_data = new Object()
+            faction_data.name = data['factions'][i]['name']
+            faction_data.data = influences
+            series.push(faction_data)
+        }
+        console.log(series)
+        return series
+    })
+    .fail(function() {
+        console.log('erreur')
+    })
+}
 
 function draw_graph()
 {
 
-    console.log(Highcharts.chart('container', {
+    Highcharts.chart('container', {
 
         title: {
             text: 'Influence du système'
@@ -34,7 +64,7 @@ function draw_graph()
             }
         },
 
-        series:   get_BGSdata_from_API(),
+        series: get_BGSdata_from_API(),
 
         responsive: {
             rules: [{
@@ -52,45 +82,17 @@ function draw_graph()
         }
 
     }
-    ));
+    )
+    console.log(Highcharts.chart)
 }
 
 
 
-function get_BGSdata_from_API()
-{
-    $.get("https://www.edsm.net/api-system-v1/factions?systemName=Karka&showHistory=1", {})
-    .done(function(data)
-    {
-        series = [];
-        for(var i = 0; i <= Object.keys(data).length; i++) 
-        {
-           influences_Data = data['factions'][i]['influenceHistory'];
-           influences = [];
 
-
-           for(var j in influences_Data)
-           {
-            influences.push(influences_Data [j] * 100);
-        }
-        influences = influences.slice(influences.length - nbrvalue);
-        series.push( [['name',data['factions'][i]['name']], ['data',influences]] );
-
-    }
-
-            console.log(series);
-
-
-    return series;
-})
-    .fail(function() {
-        console.log('erreur');
-    });
-}
 
 
 
 $(document).ready(function()
 {
-    draw_graph();
-});
+    draw_graph()
+})
